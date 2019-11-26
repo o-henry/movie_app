3b01da2022b42bb6ad23cdce247c6ab8da117340
Chan Haeng Lee <elt@Chanui-MacBookPro.local>
2019-11-26 12:43:50 +0900

    Updates

---
+++ precache-manifest.0818a848e2e827cac2cbdb475b0e6cef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcf9fe1a76fc3101de6479804d3a0202",
+    "url": "/moive_app/index.html"
+  },
+  {
+    "revision": "97aca9414882073fe7b0",
+    "url": "/moive_app/static/css/main.b9bd5515.chunk.css"
+  },
+  {
+    "revision": "6f0599dff9eecd2a0e73",
+    "url": "/moive_app/static/js/2.43e6c5f9.chunk.js"
+  },
+  {
+    "revision": "97aca9414882073fe7b0",
+    "url": "/moive_app/static/js/main.d73546b4.chunk.js"
+  },
+  {
+    "revision": "b6bb56da380c3188df62",
+    "url": "/moive_app/static/js/runtime-main.aea548c8.js"
+  }
+]);
